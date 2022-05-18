a48f36b06ab5ceee13f7a623b5a405eab24242cb
jm_ps <ymail.com>
2022-05-18 21:05:12 +0200

    Redistribucion

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Mascota } from '../shared/interface';
-import { DataService } from '../shared/data.service';
+import { Mascota } from '../../shared/interface';
+import { DataService } from '../../shared/data.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
