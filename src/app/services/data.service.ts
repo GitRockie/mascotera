467e2e3ecd852630a3fb7d5ed8e4c5daf7eac69b
Roman Kostyuchenko <elromankos@gmail.com>
2022-05-20 00:33:27 +0200

    Firestore service
    BBDD creation BTL

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
+import { collection } from '@firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor( private fireStore: Firestore ) { }
+
+  getNotes() {
+    const notesRef = collection( this.fireStore, 'notes' );
+    return collectionData(notesRef);
+  }
 }
