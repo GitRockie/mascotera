60d8a48c0f3dc1bb5927f055d742ab917687809b
Roman Kostyuchenko <elromankos@gmail.com>
2022-05-20 00:22:18 +0200

    Firebase updates app module/Connection Create
    Firestore imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { environment } from '../environments/environment';
+
+import {provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import {getFirestore, provideFirestore } from '@angular/fire/firestore'
+
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, 
+    provideFirebaseApp( () => initializeApp( environment.firebase )),
+    provideFirestore (() => getFirestore())
+
+  
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
