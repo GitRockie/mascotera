6b1abf9c03a57450faccca24222cf5d0f2ea008f
ivannietoginebro <Ivannieto@ginebro.cat>
2022-05-20 22:31:49 +0200

    COMMIT

--- src/app/pages/tab2/tab2.module.ts
+++ src/app/pages/tab2/tab2.module.ts
@@ -1,17 +1,17 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
 import { ExploreContainerComponentModule } from '../../explore-container/explore-container.module';
-
 import { Tab2PageRoutingModule } from './tab2-routing.module';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
+    ReactiveFormsModule,
     FormsModule,
     ExploreContainerComponentModule,
     Tab2PageRoutingModule
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   firebase: {
-    projectId: 'my-project-1500015070572',
+    /* projectId: 'my-project-1500015070572',
     appId: '1:158668683934:web:013726c862cb63b073d98f',
     databaseURL: 'https://my-project-1500015070572-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'my-project-1500015070572.appspot.com',
@@ -12,7 +12,14 @@ export const environment = {
     apiKey: 'AIzaSyAQDOY24SY3vPpCkstm0drsSnysQndaY0w',
     authDomain: 'my-project-1500015070572.firebaseapp.com',
     messagingSenderId: '158668683934',
-    measurementId: 'G-4YKSMKHEJS',
+    measurementId: 'G-4YKSMKHEJS', */
+    apiKey: 'AIzaSyBVgrbRclnEYZUeWpZ-6k84BqOnxJZDiE0',
+    authDomain: 'mascotescifo.firebaseapp.com',
+    databaseURL: 'https://mascotescifo-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'mascotescifo',
+    storageBucket: 'mascotescifo.appspot.com',
+    messagingSenderId: '275895760450',
+    appId: '1:275895760450:web:60cbbf5cfe02f7204f1fdb'
   },
   production: false
 };
