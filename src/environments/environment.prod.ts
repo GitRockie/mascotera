b45db6af4949f3a6f41a6581c9094f66cf42ceed
jm_ps <ymail.com>
2022-05-28 07:49:10 +0200

    cambio proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'my-project-1500015070572',
-    appId: '1:158668683934:web:013726c862cb63b073d98f',
-    databaseURL: "https://my-project-1500015070572-default-rtdb.europe-west1.firebasedatabase.app",
-    storageBucket: 'my-project-1500015070572.appspot.com',
-    locationId: 'europe-west2',
-    apiKey: 'AIzaSyAQDOY24SY3vPpCkstm0drsSnysQndaY0w',
-    authDomain: 'my-project-1500015070572.firebaseapp.com',
-    messagingSenderId: '158668683934',
-    measurementId: 'G-4YKSMKHEJS',
-  },
+    apiKey: 'AIzaSyDODQwkk_X6ibF5MM-N1wqjndhsib-xecY',
+    authDomain: 'mascotas-d4066.firebaseapp.com',
+    databaseURL: 'https://mascotas-d4066-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'mascotas-d4066',
+    storageBucket: 'mascotas-d4066.appspot.com',
+    messagingSenderId: '239710763571',
+    appId: '1:239710763571:web:e0bcdbc03d74bba54e0146'
+    },
 production: false,
 };
