3b4e857e667a9b620424db3a8244e6eefd4e55a1
jm_ps <ymail.com>
2022-06-10 11:10:22 +0200

    Cambio Geolocation

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,8 +1,16 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  NgZone,
+} from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
 import { GoogleMap } from '@capacitor/google-maps';
 import { environment } from 'src/environments/environment';
 import { Share } from '@capacitor/share';
+import { Capacitor } from '@capacitor/core';
+import { UserService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-home',
@@ -13,16 +21,28 @@ export class Tab1Page implements OnInit {
   @ViewChild('map')
   mapRef: ElementRef<HTMLElement>;
   newMap: GoogleMap;
-  posLat =    33.6;
+  coordinate: any;
+  disponible = true;
+  posLat = 33.6;
   posLong = -117.9;
 
-  constructor() {}
+  constructor(private userService: UserService, private zone: NgZone) {}
 
   ngOnInit(): void {
-    this.currentLocation();
- //   this.createMap();
+    this.requestPermissions();
+    if (this.disponible) {
+      this.getCurrentCoordinate();
+      this.createMap();
+    }
   }
 
+  async requestPermissions() {
+    const permResult = await Geolocation.requestPermissions();
+    console.log('Perm request result: ', permResult);
+    this.disponible = permResult != null;
+    return;
+  }
+  /*
   currentLocation() {
     const currentPosition = async () => {
       const coordinates = await Geolocation.getCurrentPosition();
@@ -31,14 +51,54 @@ export class Tab1Page implements OnInit {
       console.log('Current position:', coordinates);
     };
   };
-  async geoShare(){
-    const gShare = await Share.share({
-      title: 'Mi Posicion',
-      text: 'Lat: ' + `[$posLat]` +', Long: ' + `[$posLong]`,
-//      url: 'http://ionicframework.com/',
-      dialogTitle: 'Esta es mi Posicion',
+*/
+  getCurrentCoordinate() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      this.userService.presentToast('Geolocalizacion NO DISPONBLE');
+      console.log('Plugin geolocation no disponible');
+      return;
+    }
+    Geolocation.getCurrentPosition()
+      .then((data) => {
+        this.coordinate = {
+          latitude: data.coords.latitude,
+          longitude: data.coords.longitude,
+          accuracy: data.coords.accuracy,
+        };
+      })
+      .catch((err) => {
+        this.userService.presentToast(err);
+        console.error(err);
+      });
+  }
+  /*
+  watchPosition() {
+    try {
+      this.watchId = Geolocation.watchPosition({}, (position, err) => {
+        console.log('Watch', position);
+        this.zone.run(() => {
+          this.watchCoordinate = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+        });
+        this.posLat = position.coords.latitude;
+        this.posLong = position.coords.longitude;
+      });
+    } catch (e) {
+      this.userService.presentToast(e);
+      console.error(e);
+    }
+  }
+*/
+  async geoShare() {
+    await Share.share({
+      title: 'Localizacion',
+      text: 'Esta es mi Posicion: [´$postLat´], [´$postLong´]',
+      url: 'http://ionicframework.com/',
+      dialogTitle: 'Compartin Ubicacion',
     });
-}
+  }
   async createMap() {
     this.newMap = await GoogleMap.create({
       id: 'myMap',
@@ -50,6 +110,12 @@ export class Tab1Page implements OnInit {
           lng: this.posLong,
         },
         zoom: 8,
+      },
+    });
+    const markerId = await this.newMap.addMarker({
+      coordinate: {
+        lat: this.posLat,
+        lng: this.posLong,
       },
     });
   }
