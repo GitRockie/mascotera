3bcebdd0e1d4210971f1cce7148e56d77a684b00
jm_ps <ymail.com>
2022-06-20 19:25:03 +0200

    Correcciones APIS

--- android/app/src/main/java/io/home/mascotera/MainActivity.java
+++ android/app/src/main/java/io/home/mascotera/MainActivity.java
@@ -1,22 +1,5 @@
 package io.home.mascotera;
- 
-import android.os.Bundle;
- 
+
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
- 
-import java.util.ArrayList;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
- 
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
- 
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      add(GoogleAuth.class);
-    }});
-  }
-}
+
+public class MainActivity extends BridgeActivity {}
