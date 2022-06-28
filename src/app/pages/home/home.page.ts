cb79d030cbb04b925cb824b508eea3b9a67008e4
Roman Kostyuchenko <elromankos@gmail.com>
2022-06-28 23:36:41 +0200

    HomePage building continue
    Menu works

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { Componentes } from 'src/app/models/interface';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  componentes: Observable<Componentes[]>
+
+  constructor( private menuCtrl: MenuController,
+    private dataService: DataService ) { }
 
   ngOnInit() {
+    this.componentes = this.dataService.getMenuOpt();
+  }
+
+  showMenu() {
+    this.menuCtrl.open('first');
   }
 
 }
